03082a1551fb6932278814b532c7c81c02ed1650
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2020-11-25 22:19:29 +0900

    try one more

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -17,8 +17,8 @@ public class HelloworldApplication {
     }
   }
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloworldApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(HelloworldApplication.class, args);
+  }
 
 }
